607561019885840439eb3e6285fab3f2db1febdb
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-09-18 15:29:33 +1200

    chore: restructure project and create components

---
+++ src/pages/Main/components/Searchbar/Searchbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import style from './Searchbar.module.scss';
+
+function Searchbar() {
+  return (<h1 className={style.main}>Search Bar</h1>);
+}
+
+export default Searchbar;
