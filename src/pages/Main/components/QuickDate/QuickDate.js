b21c157cfe62c19969963b64fe6f07c7e1e93cc7
Sebin Benjamin <sebinbenjamin@gmail.com>
2020-10-13 12:00:49 +1300

    improve: quickfix color cue and selected color

--- src/pages/Main/components/QuickDate/QuickDate.js
+++ src/pages/Main/components/QuickDate/QuickDate.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 import style from './QuickDate.module.scss';
+
+const useStyles = makeStyles({
+  buttonColor: {
+    '&.Mui-selected': {
+      backgroundColor: '#3284FF',
+      '&:hover': { backgroundColor: '#9DBFF2' },
+      color: 'white',
+    },
+    '&:hover': { backgroundColor: '#9DBFF2' },
+  },
+});
 
 function QuickDate() {
   const [yearToggleVal, setYearToggleVal] = React.useState('all');
@@ -11,23 +23,37 @@ function QuickDate() {
     setYearToggleVal(toggleVal);
     console.log(toggleVal);
   };
+  const classes = useStyles();
 
   return (
     <>
       <Box className={style.main}>
         <ToggleButtonGroup
+          thumbSwitchedStyle={{ backgroundColor: 'red' }}
           value={yearToggleVal}
           exclusive
           onChange={handleYearToggle}
           aria-label="Year quick select"
         >
-          <ToggleButton value="all" aria-label="left aligned">
+          <ToggleButton
+            className={classes.buttonColor}
+            value="all"
+            aria-label="left aligned"
+          >
             All years
           </ToggleButton>
-          <ToggleButton value="10" aria-label="centered">
+          <ToggleButton
+            className={classes.buttonColor}
+            value="10"
+            aria-label="centered"
+          >
             Last 10 years
           </ToggleButton>
-          <ToggleButton value="5" aria-label="right aligned">
+          <ToggleButton
+            className={classes.buttonColor}
+            value="5"
+            aria-label="right aligned"
+          >
             Last 5 years
           </ToggleButton>
         </ToggleButtonGroup>
